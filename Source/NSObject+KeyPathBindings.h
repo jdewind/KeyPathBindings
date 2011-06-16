311fb58da6527965cb4651d1d2e7ce2e15d22938
Justin DeWind <dewind@atomicobject.com>
2011-06-16 08:29:03 -0400

    Renamed project and added MAZeroingWeakRef as a submodule

--- Source/NSObject+KeyPathBindings.h
+++ Source/NSObject+KeyPathBindings.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-#define USE_ZEROING_WEAK_REFERENCES 1
+#define USE_ZEROING_WEAK_REFERENCES 0
 
 @interface NSObject(KeyPathBindings)
 - (void)bindProperty:(NSString *)property onTarget:(id)target toKeyPath:(NSString *)keyPath;
